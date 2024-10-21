e5085d0714ad17c6802712bcc3626381d77dd021
andresp2805 <a.pereiraa@uniandes.edu.co>
2024-10-21 01:24:20 -0500

    commit

--- src/data.ts
+++ src/data.ts
@@ -18,5 +18,17 @@ export const series =
     "https://www.imdb.com/title/tt1475582/?ref_=nv_sr_srsg_0_tt_7_nm_1_in_0_q_sherl", "https://m.media-amazon.com/images/M/MV5BNTQzNGZjNDEtOTMwYi00MzFjLWE2ZTYtYzYxYzMwMjZkZDc5XkEyXkFqcGc@._V1_.jpg"),
     
   new Serie("A Very English Scandal", "BBC", 2, "A Very English Scandal is a fact-based three-part British television comedy-drama miniseries.", 
-    "https://www.imdb.com/title/tt6938856/", "https://m.media-amazon.com/images/M/MV5BNTQ5NDI0MTQ0MV5BMl5BanBnXkFtZTgwNDEzNTc1NTM@._V1_.jpg")
+    "https://www.imdb.com/title/tt6938856/", "https://m.media-amazon.com/images/M/MV5BNTQ5NDI0MTQ0MV5BMl5BanBnXkFtZTgwNDEzNTc1NTM@._V1_.jpg"),
+
+  new Serie("The Crown", "Netflix", 4, "The Crown is a historical drama streaming television series about the reign of Queen Elizabeth II.",
+    "https://www.imdb.com/title/tt4786824/", "https://m.media-amazon.com/images/M/MV5BODcyODZlZDMtZGE0Ni00NjBhLWJlYTAtZDdlNWY3MzkwMGVhXkEyXkFqcGc@._V1_.jpg"),
+
+  new Serie("The Mandalorian", "Disney+", 2, "The Mandalorian is an American space Western television series created by Jon Favreau.",
+    "https://www.imdb.com/title/tt8111088/", "https://m.media-amazon.com/images/M/MV5BNjgxZGM0OWUtZGY1MS00MWRmLTk2N2ItYjQyZTI1OThlZDliXkEyXkFqcGc@._V1_.jpg"),
+
+  new Serie("The Witcher", "Netflix", 2, "The Witcher is a Polish-American fantasy drama series produced by Lauren Schmidt Hissrich.",
+    "https://www.imdb.com/title/tt5180504/", "https://m.media-amazon.com/images/M/MV5BMTQ5MDU5MTktMDZkMy00NDU1LWIxM2UtODg5OGFiNmRhNDBjXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg"),
+
+  new Serie("The Sopranos", "HBO", 6, "The Sopranos is an American crime drama television series created by David Chase.",
+    "https://www.imdb.com/title/tt0141842/", "https://m.media-amazon.com/images/M/MV5BMjRmMTNiMTQtMDg1ZS00MGM1LWE1MGUtYjEzMGFjNWUzOWRkXkEyXkFqcGc@._V1_.jpg")
 ];
